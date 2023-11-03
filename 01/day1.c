da89f2462ced39d35daed97da025e6eddf84b041
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-11-03 18:42:13 +0100

    unfinished day 1
    almost

--- 01/day1.c
+++ 01/day1.c
@@ -3,8 +3,37 @@
 #include <string.h>
 
 // Function to decode the messages
+bool isWordPresent(char *message, char *word)
+{
+    bool found = false;
+    char *token = strtok(message, " ");
+    while (token != NULL)
+    {
+        if (strcmp(token, word) == 0)
+        {
+            printf("The word '%s' is present in the sentence.\n", word);
+            found = true;
+        }
+        token = strtok(NULL, " ");
+    }
+    return found;
+}
+
 void decodeMessages(char *message)
 {
+    char solution[100];
+    int count = 0;
+
+    for (int i = 0; i < strlen(message); i++)
+    {
+        if (isWordPresent(message, "your_word_here"))
+        { // Replace "your_word_here" with the word you want to check
+            solution[count] = message[i];
+            count++;
+        }
+    }
+    solution[count] = '\0'; // Add the null terminator at the end
+    printf("Decoded message: %s\n", solution);
 }
 
 int main()
