f0155f555b1ef4c66ea4c9af4ad66b323b07fe3a
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-11-04 20:55:44 +0100

    day 1 finnished

--- 01/day1.c
+++ 01/day1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
-#include <unistd.h>
-#include <stdbool.h>
 
-void convertToLower(char *str)
+#define MAX_WORD_LENGTH 100
+#define MAX_WORDS 100
+
+void toLower(char *str)
 {
     while (*str)
     {
@@ -14,124 +15,55 @@ void convertToLower(char *str)
     }
 }
 
-bool isWordPresent(char *message, char *word)
+struct WordCount
 {
-    bool found = false;
-    char *token = strtok(message, " ");
-    while (token != NULL)
-    {
-        char *token_lower = strdup(token);
-        convertToLower(token_lower);
-        char *word_lower = strdup(word);
-        convertToLower(word_lower);
-        if (strcmp(token_lower, word_lower) == 0)
-        {
-            found = true;
-            break;
-        }
-        free(token_lower);
-        free(word_lower);
-        token = strtok(NULL, " ");
-    }
-    return found;
-}
-
-void readWordsFromFile(char *filename, char words[100][100], int *num_words)
-{
-    FILE *file = fopen(filename, "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error opening words file\n");
-        exit(1);
-    }
-
-    char word[100];
-    while (fscanf(file, "%s", word) != EOF)
-    {
-        strcpy(words[*num_words], word);
-        (*num_words)++;
-    }
-    fclose(file);
-}
-
-void decodeMessages(char *message, char words[100][100], int num_words)
-{
-    int counts[100] = {0};
-    char solution[1000] = "";
-    char *token = strtok(message, " ");
-    while (token != NULL)
-    {
-        char *token_lower = strdup(token);
-        convertToLower(token_lower);
-        for (int i = 0; i < num_words; i++)
-        {
-            char *word_lower = strdup(words[i]);
-            convertToLower(word_lower);
-            if (strcmp(token_lower, word_lower) == 0)
-            {
-                counts[i]++;
-            }
-            free(word_lower);
-        }
-        free(token_lower);
-        token = strtok(NULL, " ");
-    }
-
-    for (int i = 0; i < num_words; i++)
-    {
-        if (counts[i] > 0)
-        {
-            char count_str[10];
-            sprintf(count_str, "%d", counts[i]);
-            strcat(solution, words[i]);
-            strcat(solution, count_str);
-        }
-    }
-
-    printf("Decoded message: %s\n", solution);
-}
-
-void printCurrentDirectory()
-{
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-    {
-        printf("Current working directory: %s\n", cwd);
-    }
-    else
-    {
-        perror("getcwd() error");
-    }
-}
+    char word[MAX_WORD_LENGTH];
+    int count;
+};
 
 int main()
 {
     FILE *file;
-    char *filename = "words.txt";
-    char message[1000];
-    printCurrentDirectory();
+    char word[MAX_WORD_LENGTH];
+    char filename[] = "words.txt";
+    struct WordCount words[MAX_WORDS];
+    int wordCount = 0;
 
     file = fopen(filename, "r");
 
     if (file == NULL)
     {
-        fprintf(stderr, "Error opening file\n");
+        printf("Could not open file %s", filename);
         return 1;
     }
 
-    if (fgets(message, sizeof(message), file) == NULL)
+    while (fscanf(file, "%s", word) == 1)
     {
-        fprintf(stderr, "Error reading file\n");
-        return 1;
+        toLower(word);
+        int found = 0;
+        for (int i = 0; i < wordCount; i++)
+        {
+            if (strcmp(words[i].word, word) == 0)
+            {
+                words[i].count++;
+                found = 1;
+                break;
+            }
+        }
+        if (!found)
+        {
+            strcpy(words[wordCount].word, word);
+            words[wordCount].count = 1;
+            wordCount++;
+        }
     }
 
     fclose(file);
 
-    char words[100][100];
-    int num_words = 0;
-    readWordsFromFile("words.txt", words, &num_words);
-
-    decodeMessages(message, words, num_words);
+    for (int i = 0; i < wordCount; i++)
+    {
+        printf("%s%d", words[i].word, words[i].count);
+    }
 
     return 0;
 }
