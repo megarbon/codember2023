945b8ca7cc04b1c88d4bebfe14762837a2f21a96
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-11-04 16:48:48 +0100

    still nothing

--- 01/day1.c
+++ 01/day1.c
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 #include <stdbool.h>
 
-// Function to decode the messages
+void convertToLower(char *str)
+{
+    while (*str)
+    {
+        *str = tolower(*str);
+        str++;
+    }
+}
+
 bool isWordPresent(char *message, char *word)
 {
     bool found = false;
@@ -12,20 +21,13 @@ bool isWordPresent(char *message, char *word)
     while (token != NULL)
     {
         char *token_lower = strdup(token);
-        for (int i = 0; token_lower[i]; i++)
-        {
-            token_lower[i] = tolower(token_lower[i]);
-        }
+        convertToLower(token_lower);
         char *word_lower = strdup(word);
-        for (int i = 0; word_lower[i]; i++)
-        {
-            word_lower[i] = tolower(word_lower[i]);
-        }
-
+        convertToLower(word_lower);
         if (strcmp(token_lower, word_lower) == 0)
         {
-            printf("The word '%s' is present in the sentence.\n", word);
             found = true;
+            break;
         }
         free(token_lower);
         free(word_lower);
@@ -34,28 +36,80 @@ bool isWordPresent(char *message, char *word)
     return found;
 }
 
-void decodeMessages(char *message, char *words[], int num_words)
+void readWordsFromFile(char *filename, char words[100][100], int *num_words)
 {
-    char solution[1000];
-    int count = 0;
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error opening words file\n");
+        exit(1);
+    }
+
+    char word[100];
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        strcpy(words[*num_words], word);
+        (*num_words)++;
+    }
+    fclose(file);
+}
+
+void decodeMessages(char *message, char words[100][100], int num_words)
+{
+    int counts[100] = {0};
+    char solution[1000] = "";
+    char *token = strtok(message, " ");
+    while (token != NULL)
+    {
+        char *token_lower = strdup(token);
+        convertToLower(token_lower);
+        for (int i = 0; i < num_words; i++)
+        {
+            char *word_lower = strdup(words[i]);
+            convertToLower(word_lower);
+            if (strcmp(token_lower, word_lower) == 0)
+            {
+                counts[i]++;
+            }
+            free(word_lower);
+        }
+        free(token_lower);
+        token = strtok(NULL, " ");
+    }
 
     for (int i = 0; i < num_words; i++)
     {
-        if (isWordPresent(message, words[i]))
+        if (counts[i] > 0)
         {
-            solution[count] = *words[i];
-            count++;
+            char count_str[10];
+            sprintf(count_str, "%d", counts[i]);
+            strcat(solution, words[i]);
+            strcat(solution, count_str);
         }
     }
-    solution[count] = '\0'; // Add the null terminator at the end
+
     printf("Decoded message: %s\n", solution);
+}
+
+void printCurrentDirectory()
+{
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        printf("Current working directory: %s\n", cwd);
+    }
+    else
+    {
+        perror("getcwd() error");
+    }
 }
 
 int main()
 {
     FILE *file;
-    char *filename = "CHANGELOG_01.txt"; // File with messages
+    char *filename = "words.txt";
     char message[1000];
+    printCurrentDirectory();
 
     file = fopen(filename, "r");
 
@@ -73,8 +127,9 @@ int main()
 
     fclose(file);
 
-    char *words[] = {"word1", "word2", "word3"}; // Replace with the actual words from the file
-    int num_words = sizeof(words) / sizeof(words[0]);
+    char words[100][100];
+    int num_words = 0;
+    readWordsFromFile("words.txt", words, &num_words);
 
     decodeMessages(message, words, num_words);
 
